8e65f944932c6ee80f6bdd4eae1d65252be14d28
DmitrijPikta <dmitrij.pikta@gmail.com>
2025-04-07 13:50:33 +0300

    Student class for list

--- List/src/functions.cpp
+++ List/src/functions.cpp
@@ -16,16 +16,16 @@ void Get_final_mark(list<Stud> &grupe, bool for_average_homework_mark, bool for_
 	{
 		if (for_average_homework_mark && for_both_homework_mark)
 		{
-			student.final_mark = 0.6 * student.exam_mark + 0.4 * Get_average_for_homework_mark(student);
-			student.second_final_mark = 0.6 * student.exam_mark + 0.4 * Get_mediana_for_homework_mark(student);
+			student.set_final_mark(0.6 * student.get_exam_mark() + 0.4 * student.Get_average_for_homework_mark());
+			student.set_second_final_mark(0.6 * student.get_exam_mark() + 0.4 * student.Get_mediana_for_homework_mark());
 		}
 		else if (for_average_homework_mark)
 		{
-			student.final_mark = 0.6 * student.exam_mark + 0.4 * Get_average_for_homework_mark(student);
+			student.set_final_mark(0.6 * student.get_exam_mark() + 0.4 * student.Get_average_for_homework_mark());
 		}
 		else
 		{
-			student.final_mark = 0.6 * student.exam_mark + 0.4 * Get_mediana_for_homework_mark(student);
+			student.set_final_mark(0.6 * student.get_exam_mark() + 0.4 * student.Get_mediana_for_homework_mark());
 		}
 	}
 	//-----------------------------------------------------------------
@@ -35,11 +35,11 @@ void Get_final_mark(list<Stud> &grupe, bool for_average_homework_mark, bool for_
 	//-----------------------------------------------------------------
 }
 
-double Get_average_for_homework_mark(Stud student)
+double Stud::Get_average_for_homework_mark()
 {
 	int suma_of_marks = 0;
 	int amount_of_marks = 0;
-	for (int mark : student.Homework_marks)
+	for (int mark : Homework_marks_)
 	{
 		suma_of_marks += mark;
 		amount_of_marks++;
@@ -51,12 +51,12 @@ double Get_average_for_homework_mark(Stud student)
 	return (double)suma_of_marks / amount_of_marks;
 }
 
-double Get_mediana_for_homework_mark(Stud student)
+double Stud::Get_mediana_for_homework_mark()
 {
-	student.Homework_marks.sort();
+	Homework_marks_.sort();
 
-	auto it = student.Homework_marks.begin();
-	int amount_of_marks = student.Homework_marks.size();
+	auto it = Homework_marks_.begin();
+	int amount_of_marks = Homework_marks_.size();
 	if (amount_of_marks == 0)
 	{
 		return 0;
@@ -92,7 +92,7 @@ void Print_final_mark(list<Stud> &grupe, bool for_average_homework_mark, bool fo
 	};
 
 	string filename;
-	if (grupe.back().final_mark >= 5)
+	if (grupe.back().get_final_mark() >= 5)
 	{
 		filename = "Best_grupe.txt";
 	}
@@ -154,7 +154,7 @@ void Print_final_mark(list<Stud> &grupe, bool for_average_homework_mark, bool fo
 	{
 		for (Stud student : grupe)
 		{
-			oss << left << setw(size) << student.second_name << setw(size) << student.name << fixed << setprecision(2) << student.final_mark << endl;
+			oss << left << setw(size) << student.get_last_name() << setw(size) << student.get_name() << fixed << setprecision(2) << student.get_final_mark() << endl;
 			output = oss.str();
 			oss.str(""); // Clears the string content
 			oss.clear(); // Reset error flags (e.g., EOF)
@@ -165,7 +165,7 @@ void Print_final_mark(list<Stud> &grupe, bool for_average_homework_mark, bool fo
 	{
 		for (Stud student : grupe)
 		{
-			oss << left << setw(size) << student.second_name << setw(size) << student.name << fixed << setprecision(2) << setw(17) << student.final_mark << student.second_final_mark << endl;
+			oss << left << setw(size) << student.get_last_name() << setw(size) << student.get_name() << fixed << setprecision(2) << setw(17) << student.get_final_mark() << student.get_second_final_mark() << endl;
 			output = oss.str();
 			oss.str("");
 			oss.clear();
@@ -185,15 +185,15 @@ int Get_size_for_string_printing(list<Stud> &grupe)
 	int max_length_of_string = 0;
 	for (Stud student : grupe)
 	{
-		if (student.name.length() > max_length_of_string || student.second_name.length() > max_length_of_string)
+		if (student.get_name().length() > max_length_of_string || student.get_last_name().length() > max_length_of_string)
 		{
-			if (student.name.length() > student.second_name.length())
+			if (student.get_name().length() > student.get_last_name().length())
 			{
-				max_length_of_string = student.name.length();
+				max_length_of_string = student.get_name().length();
 			}
 			else
 			{
-				max_length_of_string = student.second_name.length();
+				max_length_of_string = student.get_last_name().length();
 			}
 		}
 	}
@@ -208,16 +208,16 @@ int Get_size_for_string_printing(list<Stud> &grupe)
 	}
 }
 
-void generate_marks(Stud &student)
+void Stud::generate_marks()
 {
 	std::random_device rd;							   // Create a random device and seed the generator
 	std::mt19937 gen(rd());							   // Mersenne Twister engine
 	std::uniform_int_distribution<int> distrib(1, 10); // Define range 1 to 10
 	for (int i = 0; i < distrib(gen); i++)
 	{
-		student.Homework_marks.push_back(distrib(gen));
+		Homework_marks_.push_back(distrib(gen));
 	}
-	student.exam_mark = distrib(gen);
+	exam_mark_ = distrib(gen);
 }
 
 void generate_marks(list<int> &Marks, int number_of_marks)
@@ -231,7 +231,7 @@ void generate_marks(list<int> &Marks, int number_of_marks)
 	}
 }
 
-void generate_name(Stud &student)
+void Stud::generate_name()
 {
 	list<string> Names = {
 		"Alice", "Bob", "Charlie", "David", "Emma",
@@ -250,11 +250,11 @@ void generate_name(Stud &student)
 
 	auto it = Names.begin();
 	std::advance(it, distrib(gen));
-	student.name = *it;
+	name_ = *it;
 
 	it = Second_names.begin();
 	std::advance(it, distrib(gen));
-	student.second_name = *it;
+	last_name_ = *it;
 }
 
 void Sort_students(list<Stud> &grupe, string parametr)
@@ -265,22 +265,22 @@ void Sort_students(list<Stud> &grupe, string parametr)
 	if (parametr == "final_mark")
 	{
 		grupe.sort([](const Stud &a, const Stud &b)
-				   { return a.final_mark > b.final_mark; });
+				   { return a.get_final_mark() > b.get_final_mark(); });
 	}
 	else if (parametr == "second_final_mark")
 	{
 		grupe.sort([](const Stud &a, const Stud &b)
-				   { return a.second_final_mark > b.second_final_mark; });
+				   { return a.get_second_final_mark() > b.get_second_final_mark(); });
 	}
 	else if (parametr == "name")
 	{
 		grupe.sort([](const Stud &a, const Stud &b)
-				   { return a.name > b.name; });
+				   { return a.get_name() > b.get_name(); });
 	}
 	else if (parametr == "second_name")
 	{
 		grupe.sort([](const Stud &a, const Stud &b)
-				   { return a.second_name > b.second_name; });
+				   { return a.get_last_name() > b.get_last_name(); });
 	}
 	//-----------------------------------------------------------------
 	auto end = std::chrono::high_resolution_clock::now();
@@ -348,7 +348,7 @@ void Divide_for_two_grupse(list<Stud> &grupe, list<Stud> &best_grupe, list<Stud>
 	int size_of_grupe = grupe.size();
 	for (int i = 0; i < size_of_grupe; i++)
 	{
-		if (grupe.front().final_mark >= 5)
+		if (grupe.front().get_final_mark() >= 5)
 		{
 			best_grupe.push_back(grupe.front());
 			grupe.pop_front();
@@ -372,7 +372,7 @@ void Divide_for_two_grupse(list<Stud> &grupe, list<Stud> &worst_grupe)
 	auto start = std::chrono::high_resolution_clock::now(); // Time
 	//---------------------------------------------------------------------------
 	Sort_students(grupe, "final_mark");
-	while (grupe.back().final_mark < 5)
+	while (grupe.back().get_final_mark() < 5)
 	{
 		worst_grupe.push_back(grupe.back());
 		grupe.pop_back();
@@ -390,10 +390,10 @@ void Divide_for_two_grupse_v3(list<Stud> &grupe, list<Stud> &best_grupe, list<St
 	auto start = std::chrono::high_resolution_clock::now(); // Time
 	//---------------------------------------------------------------------------
 	std::copy_if(grupe.begin(), grupe.end(), std::back_inserter(best_grupe), [](const Stud s)
-				 { return s.final_mark >= 5; });
+				 { return s.get_final_mark() >= 5; });
 
 	std::copy_if(grupe.begin(), grupe.end(), std::back_inserter(worst_grupe), [](const Stud s)
-				 { return s.final_mark < 5; });
+				 { return s.get_final_mark() < 5; });
 
 	grupe.clear();
 	//-----------------------------------------------------------------
@@ -449,15 +449,8 @@ void Enter_students_using_txt_file_bufer_P(list<Stud> &grupe)
 			while (getline(my_buffer, line))
 			{
 				std::stringstream ss(line);
-				ss >> student.name >> student.second_name;
-				for (int i = 0; i < number_of_homework_marks; i++)
-				{
-					ss >> mark;
-					student.Homework_marks.push_back(mark);
-				}
-				ss >> student.exam_mark;
+				Stud student(ss, number_of_homework_marks);
 				grupe.push_back(student);
-				student.Homework_marks.clear();
 			}
 
 			//-----------------------------------------------------------------
@@ -473,4 +466,20 @@ void Enter_students_using_txt_file_bufer_P(list<Stud> &grupe)
 		}
 		break;
 	}
+}
+
+Stud::Stud(std::stringstream &is, int number_of_homework_marks)
+{
+	string name, last_name;
+	int mark;
+	is >> name >> last_name;
+	name_ = name;
+	last_name_ = last_name;
+	for (int i = 0; i < number_of_homework_marks; i++)
+	{
+		is >> mark;
+		set_homework_marks(mark);
+	}
+	is >> mark;
+	set_exam_mark(mark);
 }
